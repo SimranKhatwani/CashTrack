5559ba5fac7e02032019c88b55f90fab14c90581
Simran Khatwani <simrankhatwani2807@gmail.com>
2026-06-14 17:05:40 +0530

    Changes made few file

--- node_modules/cashtrack-client/vite.config.js
+++ node_modules/cashtrack-client/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000'
+  ...(mode === 'development' && {
+    server: {
+      proxy: {
+        '/api': 'http://localhost:5000'
+      }
     }
-  }
-});
+  })
+}));
