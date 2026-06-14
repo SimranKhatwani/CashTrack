de6ad695f2b9e8d69fd156588565a13eab5dcfa3
Simran Khatwani <simrankhatwani2807@gmail.com>
2026-06-14 16:05:36 +0530

    fix backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api'
+  baseURL: 'https://cashtrack-57np.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
